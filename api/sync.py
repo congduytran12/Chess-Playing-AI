30d6fba9941835c2b649b10b04188e77439af3de
Tran Cong Duy <congduytran12@gmail.com>
2026-04-03 11:21:41 +0700

    update user-agent

--- api/sync.py
+++ api/sync.py
@@ -34,7 +34,8 @@ def sync():
     try:
         # Use a realistic User-Agent to avoid IP throttling
         headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+            # 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
             'Content-Type': 'text/plain'
         }
         
